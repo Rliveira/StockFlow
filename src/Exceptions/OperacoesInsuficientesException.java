1b2eba162cc45a3dec9d5552dd97230a47b43759
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-09-15 14:44:10 -0300

    Adicionando exception

---
+++ src/Exceptions/OperacoesInsuficientesException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class OperacoesInsuficientesException extends Exception {
+    public OperacoesInsuficientesException(String mensagem) {
+        super(mensagem);
+    }
+}
