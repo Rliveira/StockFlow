955189bfaf624096190e1591e928a8e94d3c47ea
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-09-16 20:14:42 -0300

    adicionando exceções

---
+++ src/Exceptions/HistoricoInsuficienteException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class HistoricoInsuficienteException extends Exception {
+    public HistoricoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
